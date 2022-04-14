e9b4a32c7e5f9744ad3587ef35430698d0bb401b
unknown <crsiufung@hotmail.com>
2022-04-14 12:45:33 +0800

    login with email

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ const app_id = process.env.STREAM_APP_ID;
 
 const signup = async (req, res) => {
     try {
-        const { fullName, username, password, phoneNumber } = req.body;
+        const { fullName, username, password, email } = req.body;
 
         const userId = crypto.randomBytes(16).toString("hex");
 
@@ -21,7 +21,7 @@ const signup = async (req, res) => {
 
         const token = serverClient.createUserToken(userId);
         
-        res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
+        res.status(200).json({ token, fullName, username, userId, hashedPassword, email });
     } catch (error) {
         console.log(error);
 
@@ -31,12 +31,12 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { email, password } = req.body;
 
         const serverClient = connect(api_key, api_secret, app_id);
         const client = StreamChat.getInstance(api_key, api_secret);
 
-        const { users } = await client.queryUsers({ name: username });
+        const { users } = await client.queryUsers({ email: email });
 
         if (!users.length) return res.status(400).json({ message: "User not found" });
 
@@ -45,7 +45,7 @@ const login = async (req, res) => {
         const token = serverClient.createUserToken(users[0].id);
 
         if (success) {
-            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
+            res.status(200).json({ token, fullName: users[0].fullName, username: users[0].name, userId: users[0].id});
         } else {
             res.status(500).json({ message: "incorrect password" });
         }
